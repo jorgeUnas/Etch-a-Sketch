f9b58a29a1a0825dd7a33ae952e9bf6f213f0e8f
jorgeUnas <jorgeunas@gmail.com>
2023-11-15 15:57:19 +0000

    Rainbow mode set

--- etchasketch.js
+++ etchasketch.js
@@ -1,5 +1,24 @@
 const grids = document.querySelector('#grids');
 const start = document.querySelector('#start');
+const random = document.querySelector('#random');
+const blackmode = document.querySelector('#blackMode');
+
+const black = RGBA(0,0,0,1.0);
+var rainbow = false;
+
+
+
+//rainbow activactor
+
+random.addEventListener('click', () => {
+    rainbow = true;
+});
+blackmode.addEventListener('click', () => {
+    rainbow = false;
+});
+
+
+
 
 
 start.addEventListener('click', () => {
@@ -12,20 +31,22 @@ start.addEventListener('click', () => {
         for (let i = 1; i<=numGrids; i++){
             const gridElem = document.createElement('div');
              gridElem.setAttribute('class', 'gridElem');
+                //create squares
                 for (let j = 1; j<= numGrids; j++){
                     const square = document.createElement('div');
                     square.setAttribute('class', 'square');
                     gridElem.appendChild(square);
                     
-                    // putting color on the divs
-            square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'black';
-                /*if(!gridElem.style.opacity){
-                    gridElem.style.opacity = 0.1;
-                }else {
-                    gridElem.style.opacity += 0.1
-                };  */  
-            }) 
+                    // putting color on the squares
+                    square.addEventListener('mouseover', () => {
+                        
+                        if (rainbow){  // rainbow mode
+                            square.style.backgroundColor = randomColors();
+                        }else{  // black mode
+                            square.style.backgroundColor = black;
+                        }
+                         
+                    }) 
                 };
              
             grids.appendChild(gridElem);
@@ -39,6 +60,27 @@ start.addEventListener('click', () => {
 
 })
 
+
+function RGBA(red,green,blue,alpha) {
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+    this.alpha = alpha;
+        return "rgba("+this.red+","+this.green+","+this.blue+","+this.alpha+")";
+
+}
+
+//use ramdom colors
+
+function randomColors(){
+    let red = Math.round(Math.random()*255);
+    let green = Math.round(Math.random()*255);
+    let blue = Math.round(Math.random()*255);
+    
+    return RGBA(red, green, blue, 1.0);
+}
+
+console.log(randomColors());
 
 
 
